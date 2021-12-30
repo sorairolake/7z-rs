90735dc30d5d84b8cd14e9c474c9ee1b0b026919
Shun Sakai <sorairolake@protonmail.ch>
2021-12-31 01:10:00 +0900

    Initialize Cargo package

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+// Copyright (C) 2021 Shun Sakai
+//
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
